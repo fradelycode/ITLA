990f8303dc4fb016b2de68054a9abfc58f63ede6
fradely code <fradelydventura@gmail.com>
2020-06-10 14:45:17 -0400

    suma de monedas

---
+++ 02-total-pesos/js/script.js
@@ -0,0 +1,11 @@
+/** @format */
+
+let n1 = 1; // n1 mondeas de 1 peso
+let n2 = 0.5; // n2 monedas 0.50 centavos
+let n3 = 0.25; // n3 monedas 0.25 centavos
+let n4 = 0.1; // n4 monedas 0.10 centavos
+let n5 = 0.05; // n5 monedas 0.05 centavos
+
+let total = n1 + n2 + n3 + n4 + n5;
+
+alert("La suma de las monedas es igual a :" + total);
